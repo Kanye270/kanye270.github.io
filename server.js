4f86f6f8bad78aa14d68567cce236c3c56632414
Kanye270 <nicolasflorez270@gmail.com>
2025-02-12 15:02:20 -0500

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Conectar a la base de datos MySQL
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 db.connect((err) => {
     if (err) {
