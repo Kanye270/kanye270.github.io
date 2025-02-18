d0d63c4665c167aad7dd70af77c0837d620da2a4
Kanye270 <nicolasflorez270@gmail.com>
2025-02-18 09:40:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Conectar a la base de datos MySQL
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST=kanye270githubio-production.up.railway.app,
+    user: process.env.DB_USER=root,
+    password: process.env.DB_PASSWORD=1016945145,
+    database: process.env.DB_NAME=database,
+    port: process.env.DB_PORT=3306
 });
 
 db.connect((err) => {
